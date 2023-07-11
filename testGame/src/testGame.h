bd39b7c92b894abeb6311b7d6ee2be8954fe0054
LAPTOP-TSB\user <buwanekatharindu@gmail.com>
2023-07-11 22:10:17 +0530

    added Logging

--- testGame/src/testGame.h
+++ testGame/src/testGame.h
@@ -4,6 +4,7 @@
 // For use by testGame applications
 
 #include "testGame/Application.h"
+#include "testGame/Log.h"
 
 // --- Entry Point-------------------
 #include "testGame/EntryPoint.h"
--- testGame/src/testGame/EntryPoint.h
+++ testGame/src/testGame/EntryPoint.h
@@ -7,7 +7,10 @@ extern testGame::Application* testGame::CreateApplication();
 
 int main(int argc,char** argv)
 {
-	
+	testGame::Log::Init();
+	TG_CORE_WARN("Initialized Log!");
+	int a = 8;
+	TG_INFO("Hello! Var={0}",a);
 	auto app = testGame::CreateApplication();
 	app->Run();
 	delete app;
